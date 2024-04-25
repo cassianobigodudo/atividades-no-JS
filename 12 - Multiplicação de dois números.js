c5a00177ba4c4a7cd86bdfc0b3ffba872e339d4e
Cassiano Machado <166057967+cassianobigodudo@users.noreply.github.com>
2024-04-24 22:59:38 -0300

    Add files via upload

---
+++ 12 - Multiplicação de dois números.js
@@ -0,0 +1,10 @@
+/*Faça um programa no qual o usuário digite dois números e mostre na tela a multiplicação desses números. */
+
+let numeroUm, numeroDois, resultadoNumeros
+
+numeroUm = Number(prompt('Digite seu número'))
+numeroDois = Number(prompt('Digite seu outro número'))
+
+resultadoNumeros = numeroUm*numeroDois
+
+alert(`Ao multiplicar ${numeroUm} e ${numeroDois} o resultado dá ${resultadoNumeros}`)
