0bc764f4d2e5d2e3ec298c57e5ed5379bc8e957a
Cassiano Machado <166057967+cassianobigodudo@users.noreply.github.com>
2024-04-24 22:59:03 -0300

    Add files via upload

---
+++ 07 - Maior de 18.js
@@ -0,0 +1,13 @@
+/*Solicite a idade do usuário e informe se é maior de 18 anos.*/
+
+let idade
+
+idade = Number(prompt(`Digite sua idade`))
+
+if(idade > 18){
+
+    alert(`Você é maior de 18 anos`)
+}else{
+
+    alert('Você não é maior de 18 anos')
+}
