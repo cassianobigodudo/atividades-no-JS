6e1828a4368be15f6ee62097dea90c96f4733739
Cassiano Machado <166057967+cassianobigodudo@users.noreply.github.com>
2024-05-28 14:55:02 -0300

    63 - Competição dardos.js

--- 63 - Competição dardos.js
+++ 63 - Competição dardos.js
@@ -1,43 +1,27 @@
-//todo: Em uma competição de dardos, os competidores se classificam para a próxima fase caso consigam 75% ou mais da pontuação total, que é 60 pontos. Caso consiga 50% ou mais, porém abaixo de 75%, vai para a repescagem. Menos que isso, é eliminado. Cada arremesso pode marcar de 0 a 10 pontos, dependendo da precisão. Criar um programa para armazenar a pontuação dos 6 arremessos de um competidor e ao final mostrar uma mensagem com sua pontuação, seu desempenho (percentual) e se ele está classificado, se foi para a repescagem ou se está desclassificado.
+//todo: Fazer um programa no qual o usuário deve digitar um número e o expoente dele. Mostrar na tela o resultado do cálculo. (Não é permitido usar o operador **).
 
-let somaDardos = 0, percentualRepescagem = 50, percentualClassificado = 75, pontuacaoMaxima = 60, percentualJogador
-let dardoJogado
+let numeroBase, numeroExpoente, resultadoEquacao
 
-for(i=0; i < 6; i++){
+do{
+numeroBase = Number(prompt(`Digite o número base`))
+numeroExpoente = Number(prompt(`Digite o número expoente`))
 
-    do{
+}while(isNaN(numeroBase) || isNaN(numeroExpoente))
 
-        dardoJogado = Math.floor(Number(prompt(`Digite o valor do arremeso do ${i+1}º dardo (0 a 10 pontos)`)))
+resultadoEquacao = numeroBase
 
-    }while(isNaN(dardoJogado) || dardoJogado < 0 || dardoJogado > 10)
+for(i = 1; i < numeroExpoente; i++){
 
-        somaDardos += dardoJogado
+    resultadoEquacao *= numeroBase
+    console.log(resultadoEquacao)
+
+
+}
+
+if (numeroExpoente == 0){
+
+    alert('O resultado deu 1')
 
 }
 
-percentualJogador = somaDardos * 100 / pontuacaoMaxima
-
-alert(`Sua pontuação foi ${somaDardos} pontos de 60 pontos!\n\nSeu percentual de pontos é ${percentualJogador}%`)
-
-switch(true){
-
-    case percentualJogador >= percentualClassificado:
-
-        alert(`Você foi classificado!`)
-        break
-    
-    case percentualJogador >= percentualRepescagem:
-
-        alert(`Você foi para a repescagem`)
-        break
-
-    case percentualJogador < percentualRepescagem:
-
-        alert('Você foi desclassificado')
-        break
-
-    default:
-
-        alert('ERROR 404')
-
-}
+alert(`O resultado deu `+ resultadoEquacao)
